0a29a70885b7042cd37103e8a7ef117a63b910db
croope@gmail.com <croope@gmail.com>
2017-02-10 17:44:11 +0000

    messing around

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 
 this.nodeData = [
-    {id: 1, x: 10, y: 10, label: 'Start', transitions: [{id:50, fromStateId: 1, toStateId: 2, label: 'Forward'}]},
-    {id: 2, x: 200, y: 200, label: 'End', transitions: [{id: 51, fromStateId: 2, toStateId: 1, label: 'Back'}]},
-    {id: 3, x: 500, y: 10, label: 'Middle', transitions: [{id: 52, fromStateId: 3, toStateId: 2, label: 'Onwards'}]},
-    {id: 4, x: 200, y: 10, label: 'Shire', transitions: [{id: 53, fromStateId: 4, toStateId: 3, label: 'Back Again'}]}
+    {id: 1, x: 100, y: 100, label: 'Start', transitions: [{id:50, fromStateId: 1, toStateId: 2, label: 'Forward'}]},
+    {id: 2, x: 250, y: 250, label: 'End', transitions: [{id: 51, fromStateId: 2, toStateId: 1, label: 'Back'}]},
+    {id: 3, x: 500, y: 100, label: 'Middle', transitions: [{id: 52, fromStateId: 3, toStateId: 2, label: 'Onwards'}]},
+    {id: 4, x: 250, y: 100, label: 'Shire', transitions: [{id: 53, fromStateId: 4, toStateId: 3, label: 'Back Again'}]}
 ];
+
+var rectWidth = 175;
+var rectHeight = 80;
 
 function onNodeMove(node) {
     // TODO: Add logic here to deal with node movement and redrawing of lines.
@@ -20,19 +23,19 @@ function linkPopup(link) {
 }
 
 function redrawLines() {
-    var rects = svg.selectAll('rect');
-
-    var obj = [];
-    for (a = 0; a < rects[0].length; a++) {
-        if(a > 0) {
-            var x1 = rects[0][a-1].attributes.x.value;
-            var y1 = rects[0][a-1].attributes.y.value;
-            var x2 = rects[0][a].attributes.x.value;
-            var y2 = rects[0][a].attributes.y.value;
-            var linearr = [{x:x1,y:y1},{x:x2,y:y2}];
-            obj.push(linearr);
-        }
-    }
+    // var rects = svg.selectAll('rect');
+    //
+    // var obj = [];
+    // for (a = 0; a < rects[0].length; a++) {
+    //     if(a > 0) {
+    //         var x1 = rects[0][a-1].attributes.x.value;
+    //         var y1 = rects[0][a-1].attributes.y.value;
+    //         var x2 = rects[0][a].attributes.x.value;
+    //         var y2 = rects[0][a].attributes.y.value;
+    //         var linearr = [{x:x1,y:y1},{x:x2,y:y2}];
+    //         obj.push(linearr);
+    //     }
+    // }
 
     this.svg.selectAll('.line')
         .append('path')
@@ -67,13 +70,15 @@ function setupDragAndZoom() {
     this.zoom = d3.behavior.zoom()
         .scaleExtent([0.2, 1.5])
         .on("zoom", function () {
-          d3.select('g').attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+            this.svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
         });
 }
 
 setupDragAndZoom();
 
-this.svg = d3.select('#editor').append('svg').attr('width', 1250).attr('height', 750).call(this.zoom);
+this.svg = d3.select('#editor').append('svg').attr('width', 1250).attr('height', 750);
+
+this.svg.call(zoom);
 
 this.svg.on('contextmenu', function(d) {
     //Do stuff, add menu for removing etc
@@ -84,6 +89,10 @@ this.svg.on('contextmenu', function(d) {
 this.defs = svg.append('svg:defs');
 
 function mouseDown() {
+    if (d3.event.srcElement != 'rect') {
+        return;
+    }
+
     var m = d3.mouse(this);
 
     var line = d3.selectAll('svg').append('line')
@@ -130,38 +139,39 @@ function createNodes() {
       .enter()
       .append('g')
       .call(this.drag)
+      .attr('x', function (d) {
+          return d.x;
+      })
+      .attr('y', function (d) {
+          return d.y;
+      })
       .attr('transform', function(d){
-          return 'translate(' + [d.x, d.y] + ')';
+         return 'translate(' + [d.x, d.y] + ')';
       });
 
     node.append('rect')
         .attr('rx', '20')
         .attr('ry', '20')
-        .attr('x', function (d) {
-    		return d.x;
-    	})
-    	.attr('y', function (d) {
-    		return d.y;
-    	})
-        .on('contextmenu', function(d){
+        .attr('width', rectWidth)
+        .attr('height', rectHeight)
+        .on('contextmenu', function(d) {
             d3.event.stopPropagation();
             // Do stuff here, delete edit etc
             alert(d.label);
+        })
+        .on('mousedown', mouseDown)
+        .on('mouseup', function(d) {
+            if (d3.event.srcElement != 'rect') {
+                return;
+            }
+            this.svg.on("mousemove", null);
         });
-        // .on('mousedown', mouseDown)
-        // .on('mouseup', function(d){
-        //      this.svg.on("mousemove", null);
-        // });
 
     node.append('text').text(function(d) {
             return d.label;
         })
-        .attr('x', function (d) {
-    		return d.x + 85;
-    	})
-    	.attr('y', function (d) {
-    		return d.y + 40;
-    	})
+        .attr('x', rectWidth / 2)
+    	.attr('y', rectHeight / 2)
     	.attr('text-anchor', 'middle');
 }
 
@@ -226,8 +236,20 @@ function createLinks() {
         .interpolate('basis');
 
     linkPath.attr('d', function(d){
-        return ' M ' + d.x1 + ' ' + d.y1 +
-               ' L ' + d.x2 + ' ' + d.y2 +
+        // Calculate the position for the links
+        var x1 = d.x1 + rectWidth/2;
+        var x2 = d.x2 - rectWidth/2;
+        var y1 = d.y1 + rectHeight;
+        var y2 = d.y2 - rectHeight;
+
+        // TODO: If the nodes are relatively horizontally aligned go from and to the middle
+        // if (y2 == y1 || y2 >= y1 + rectHeight/2 || y2 <= rectHeight) {
+        //     x1 = d.x1 + rectWidth;
+        //     x2 = d.x2 - rectWidth;
+        // }
+
+        return ' M ' + x1 + ' ' + y1 +
+               ' L ' + x2 + ' ' + y2 +
             //    ' L ' + d.x2 + ' ' + d.y2 +
             //    ' L ' + d.x2 + ' ' + d.y2 +
             //    ' L ' + d.x2 + ' ' + d.y2 +
